730eaafd172c90bbdb98147ffe03fdb48db97819
Beoveo <beavillegas1997@gmail.com>
2016-12-11 18:22:16 +0100

    DOC

--- src/commands/CommandParser.java
+++ src/commands/CommandParser.java
@@ -5,16 +5,19 @@ import bytecodes.AddByteCodeProgram;
 /**
  * 
  * CLASS COMMAND_PARSER:
- * Esta clase posee unicamente un metodo estatico.
- * Parsea el comando que se le introduzca.
- *
+ * Clase que comprueba de que comando se trata y devuelve un nuevo objeto de esa clase.
+ * 
  */
 
 public class CommandParser {
 	private final static Command[] commands = {new Help(),new Quit(), new Reset(),
 		 new Replace(),new Run(),new AddByteCodeProgram()};
 	
-	
+	/**
+	 * Metodo que parsea el comando, que es lo primero que se introduce por teclado.
+	 * @param line: Linea introducida por teclado.
+	 * @return Devuelve un nuevo objeto de ese comando, si coincide con alguno de los disponibles.
+	 */
 	public static Command parse(String line) { 
 		line = line.trim(); 
 		String []words = line.split(" +"); 
@@ -30,6 +33,9 @@ public class CommandParser {
 		 return c;
 		 }
 	
+	/**
+	 * Muestra la ayuda de todos los comandos.
+	 */
 	 public static void showHelp() {
 		 for (int i=0; i < CommandParser.commands.length; i++)
 		 System.out.println(CommandParser.commands[i].textHelp());
--- src/commands/Reset.java
+++ src/commands/Reset.java
@@ -2,23 +2,43 @@ package commands;
 
 import paquete.Engine;
 
+/**
+ * CLASE RESET:
+ * Clase que hereda los metodos de command, en la cual se parsea la linea introducida por teclado y contiene el metodo 
+ * para ejecutar el comando.
+ */
 public class Reset extends Command {
 	
-	 
+	/**
+	 * Metodo que ejecuta el comando.
+	 */
 	public boolean execute(Engine engine) {
 		return engine.resetProgram();
 	}
-		
+	
+	/**
+	 * Metodo que parsea el comando y comprueba cual es de
+	 * todos los disponibles.
+	 */
 	@Override
 	public Command parse(String[] s) {
 	if (s.length!=1 || !s[0].equalsIgnoreCase("RESET")) return null;
 		else return new Reset();
 	}
+	
+	/**
+	 * Metodo que muestra una ayuda para indicar al usuario la utilidad de 
+	 * cada uno.
+	 */
 	@Override
 	public String textHelp() {
 	 return " RESET: Permite resetear un programa " +
 		 System.getProperty("line.separator");
 	}
+	
+	/**
+	 * Metodo toString que devuelve el nombre del comando.
+	 */
 	public String toString(){
 		return "RESET";
 	}
